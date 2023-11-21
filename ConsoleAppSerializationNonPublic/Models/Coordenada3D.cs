cfd9a01f5fbaae691dd2eeb27ee999a968afa86d
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-21 17:26:43 -0300

    SerializationNonPublic

---
+++ ConsoleAppSerializationNonPublic/Models/Coordenada3D.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleAppSerializationNonPublic.Models;
+
+public class Coordenada3D
+{
+    [JsonConstructor]
+    internal Coordenada3D(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    [JsonInclude]
+    internal double X { get; }
+
+    [JsonInclude]
+    protected double Y { get; }
+
+    [JsonInclude]
+    private double Z { get; }
+
+    public void Show() =>   
+        Console.WriteLine($"{nameof(Coordenada3D)}.{nameof(Show)}(): " +
+            $"X = {X} | Y = {Y} | Z = {Z}");
+}
---
+++ ConsoleAppSerializationNonPublic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ConsoleAppSerializationNonPublic.Models;
+
+Console.WriteLine("***** Testes com .NET 8 | Serializacao de membros nao-publicos *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var coordenadasSerializadas = new List<string>();
+var coordenadasIniciais = new Coordenada3D[] { new(3, 0, 5), new(-5, -5, 7) };
+
+Console.WriteLine();
+Console.WriteLine("**** Iniciando serializacao...");
+foreach (var coordenada in coordenadasIniciais)
+{
+    Console.WriteLine();
+    coordenada.Show();
+    var json = JsonSerializer.Serialize(coordenada);
+    coordenadasSerializadas.Add(json);
+    Console.WriteLine($"Resultado da serializacao: {json}");
+}
+
+Console.WriteLine();
+Console.WriteLine("**** Iniciando desserializacao...");
+foreach (var json in coordenadasSerializadas)
+{
+    Console.WriteLine();
+    Console.WriteLine($"String para desserializacao: {json}");
+    var coordenada = JsonSerializer.Deserialize<Coordenada3D>(json)!;
+    coordenada.Show();
+}
